89733883e2f5a14566d6b95da253ae42e27da7ec
Michael <michaela.sifuentes@yahoo.com>
2019-02-23 16:25:25 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import FetchLocation from './components/FetchLocation'
+
+export default class App extends React.Component {
+  getUserLocationHandler = () => {
+    console.log('Pressed the button');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation={this.getUserLocationHandler}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
